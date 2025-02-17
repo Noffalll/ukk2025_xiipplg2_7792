68102790abacf85cde488d1a7b0d348c9891617f
Noffalll <143465666+Noffalll@users.noreply.github.com>
2025-02-17 13:07:56 +0700

    membuatnavbar

---
+++ vite-project/src/componens/Navbar.jsx
@@ -0,0 +1,36 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+export default function NavbarEnhance() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#Td">Todo List</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#tambahtg">Tambah tugas</Nav.Link>
+            <Nav.Link href="#lihattg">Lihat Tugas</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter,Routes,Route} from "react-router"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+ <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<App />} />
+      <Route path="/" element={<App />} />
+      
+    </Routes>
+  </BrowserRouter>
   </StrictMode>,
 )
